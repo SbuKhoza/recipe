42d10b3740430cdb8343ccce7c28a458355d47b3
Sibusiso <sibusisok59@gmail.com>
2024-08-11 21:53:42 +0200

    update

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+    return (
+        <div className='container'>
+
+            <nav>
+
+                <div className='logo'><h1>HUNGRY?</h1></div>
+
+            </nav>
+
+            <div className='banner'>
+
+
+            </div>
+
+            <div className='content'>
+
+
+            </div>
+
+            <div className='footer'>
+
+
+            </div>
+
+        </div>
+        
+    );
+};
+
+export default Home
